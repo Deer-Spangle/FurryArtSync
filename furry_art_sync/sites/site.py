a3f791231de074ac8a1bacca84872599f7b08743
Deer-Spangle <deer@spangle.org.uk>
2023-05-27 00:55:56 +0100

    Implementing gallery-dl download for generic sites, with a little bit of overrides for weasyl

--- furry_art_sync/sites/site.py
+++ furry_art_sync/sites/site.py
@@ -1,7 +1,12 @@
+import json
+import os.path
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
+import gallery_dl
+
+from furry_art_sync.datastore import Datastore
 from furry_art_sync.sites.post import Post
 
 
@@ -12,10 +17,36 @@ class SiteProfile(ABC):
         raise NotImplementedError
 
     def list_local_posts(self) -> List[Post]:
-        pass
+        pass  # TODO
+
+    def configure_gallery_dl(self) -> None:
+        pass  # Sites can optionally override this, to set other gallery dl config
+
+    def gallery_dl_postprocessors(self) -> List[Dict[str, str]]:
+        return [
+            {
+                "name": "metadata",
+                "mode": "json",
+                "filename": "{id}.json",
+            }
+        ]
 
     def download_posts(self) -> None:
-        pass
+        config_path = str(Path(Datastore.STORE_DIR) / "gallery_dl_config.json")
+        if not os.path.exists(config_path):
+            with open(config_path, "w") as f:
+                json.dump({}, f)
+        gallery_dl.config.load([config_path], strict=True)
+        gallery_dl.config.set(("extractor",), "base-directory", str(self.profile_directory()))
+        gallery_dl.config.set(("extractor",), "filename", "{id}.{extension}")
+        gallery_dl.config.set(("extractor",), "postprocessors", self.gallery_dl_postprocessors())
+        self.configure_gallery_dl()
+        data_job = gallery_dl.job.DataJob(self.profile_link())
+        dl_job = gallery_dl.job.DownloadJob(self.profile_link())
+        data_resp = data_job.run()
+        dl_resp = dl_job.run()
+        print(data_resp)
+        print(dl_resp)
 
     @abstractmethod
     def profile_directory(self) -> Path:
--- furry_art_sync/sites/site_weasyl.py
+++ furry_art_sync/sites/site_weasyl.py
@@ -1,12 +1,40 @@
+from pathlib import Path
+from typing import List, Dict
+
+import gallery_dl
+import requests
+
+from furry_art_sync.datastore import Datastore
 from furry_art_sync.sites.site import SiteProfile
 
 
 class WeasylSiteProfile(SiteProfile):  # TODO
+    SITE_DIR = "weasyl"
+
     def __init__(self, username: str) -> None:
         self.username = username
 
+    def profile_directory(self) -> Path:
+        return Path(Datastore.STORE_DIR) / self.SITE_DIR / self.username
+
     def profile_link(self) -> str:
         return f"https://weasyl.com/~{self.username}"
+
+    def validate(self) -> bool:
+        resp = requests.get(self.profile_link())
+        return resp.status_code != 404
+
+    def configure_gallery_dl(self) -> None:
+        gallery_dl.config.set(("extractor",), "filename", "{submitid}.{extension}")
+
+    def gallery_dl_postprocessors(self) -> List[Dict[str, str]]:
+        return [
+            {
+                "name": "metadata",
+                "mode": "json",
+                "filename": "{submitid}.json",
+            }
+        ]
 
     @classmethod
     def user_setup_profile(cls) -> "SiteProfile":
