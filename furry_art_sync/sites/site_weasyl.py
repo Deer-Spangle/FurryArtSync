acdbd9c8ba3ca693e547d64f68683aaaa24e3143
Deer-Spangle <deer@spangle.org.uk>
2023-05-30 00:59:10 +0100

    Using Weasyl API key, to ensure nsfw stuff is downloaded too

--- furry_art_sync/sites/site_weasyl.py
+++ furry_art_sync/sites/site_weasyl.py
@@ -27,11 +27,12 @@ class WeasylPost(Post):
         return self.metadata_raw["tags"]
 
 
-class WeasylSiteProfile(SiteProfile):  # TODO
+class WeasylSiteProfile(SiteProfile):
     SITE_DIR = "weasyl"
 
-    def __init__(self, username: str) -> None:
+    def __init__(self, username: str, api_key: str) -> None:
         self.username = username
+        self.api_key = api_key
 
     def profile_directory(self) -> Path:
         return Path(Datastore.STORE_DIR) / self.SITE_DIR / self.username
@@ -41,11 +42,28 @@ class WeasylSiteProfile(SiteProfile):  # TODO
 
     def validate(self) -> bool:
         resp = requests.get(self.profile_link())
-        return resp.status_code != 404
+        if resp.status_code == 404:
+            print("Error: Weasyl user does not exist")
+            return False
+        api_resp = requests.get(
+            "https://weasyl.com/api/whoami",
+            headers={
+                "X-Weasyl-API-Key": self.api_key
+            }
+        )
+        if api_resp.status_code != 200:
+            print("Error: Failure authenticating to Weasyl API")
+            return False
+        api_data = api_resp.json()
+        if "error" in api_data:
+            print(f"Error: Weasyl API returned error: {api_data['error']}")
+            return False
+        return True
 
     def configure_gallery_dl(self) -> None:
         gallery_dl.config.set(("extractor",), "filename", "{submitid}.{extension}")
         gallery_dl.config.set(("extractor", "weasyl"), "metadata", True)
+        gallery_dl.config.set(("extractor", "weasyl"), "api-key", self.api_key)
 
     def gallery_dl_postprocessors(self) -> List[Dict[str, str]]:
         return [
@@ -68,4 +86,5 @@ class WeasylSiteProfile(SiteProfile):  # TODO
     def user_setup_profile(cls) -> "SiteProfile":
         raw_username = input("Please enter your Weasyl username: ")
         username = raw_username.lower()
-        return cls(username)
+        api_key = input("Please go to https://www.weasyl.com/control/apikeys and generate an API key: ")
+        return cls(username, api_key)
